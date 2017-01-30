9e5585ad2d9cb2d19bc9f9edd9fc0d5f11570b26
Thomas A. McGonagle <mcgonagle@gmail.com>
2017-01-30 05:39:58 -0500

    Update exercise_1.tcl

--- exercise_1.tcl
+++ exercise_1.tcl
@@ -2,9 +2,9 @@ when CLIENT_ACCEPTED {
   set port_is_odd [expr [TCP::remote_port] & 1]
   if { $port_is_odd } {
     log local0. "Port [TCP::remote_port] is odd"
-    use pool odd
+    use pool http_pool1
   } else {
     log local0. "Port [TCP::remote_port] is even"
-    use pool even
+    use pool http_pool2
   }
 }
