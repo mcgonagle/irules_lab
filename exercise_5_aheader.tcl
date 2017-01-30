6e32f392f01f0a4fdfe34e4ed9a2041b08ebdfe6
Thomas A. McGonagle <mcgonagle@gmail.com>
2017-01-30 05:18:51 -0500

    Added xforward-for

--- exercise_5_aheader.tcl
+++ exercise_5_aheader.tcl
@@ -3,3 +3,9 @@ when HTTP_REQUEST {
               log local0. “$aHeader: [HTTP::header value $aHeader]”
         }
 }
+when HTTP_RESPONSE {
+    HTTP::header insert X-Powered-By "F5 Networks"
+    foreach aHeader [HTTP::header names] {
+           log local0. "$aHeader: [HTTP::header value $aHeader]"
+        }
+}
